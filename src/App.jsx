151b56bdc28d2211937188bfbd2bf4faa712a365
webkype <webkypecrm@gmail.com>
2025-08-20 15:36:19 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,31 @@ import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
 import "./assets/vendor/OverlayScrollbars-master/css/OverlayScrollbars.min.css";
 // import "./assets/vendor/tiny-slider/dist/tiny-slider.css";
 // import "./assets/vendor/choices.js/public/assets/styles/choices.min.css"
-import "./assets/vendor/glightbox-master/dist/css/glightbox.min.css";
-import "./assets/vendor/dropzone/dist/dropzone.css";
-import "./assets/vendor/flatpickr/dist/flatpickr.css";
-import "./assets/vendor/plyr/plyr.css";
+// import "./assets/vendor/glightbox-master/dist/css/glightbox.min.css";
+// import "./assets/vendor/dropzone/dist/dropzone.css";
+// import "./assets/vendor/flatpickr/dist/flatpickr.css";
+// import "./assets/vendor/plyr/plyr.css";
 import MainPage from "./Main-Page/MainPage";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'react-tooltip/dist/react-tooltip.css'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "react-tooltip/dist/react-tooltip.css";
 import "dropzone/dist/dropzone.css";
 import "tiny-slider/dist/tiny-slider.css";
 
-
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap-icons/font/bootstrap-icons.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "react-datepicker/dist/react-datepicker.css";
 import "flatpickr/dist/flatpickr.css";
-import 'glightbox/dist/css/glightbox.min.css';
-import 'choices.js/public/assets/styles/choices.min.css';
+import "glightbox/dist/css/glightbox.min.css";
+import "choices.js/public/assets/styles/choices.min.css";
 
 function App() {
-
   return (
     <Fragment>
-      <MainPage/>
+      <MainPage />
     </Fragment>
   );
 }
